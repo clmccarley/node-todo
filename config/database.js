ecf88959ad8d320ddc9ed6d5bfb1bc1b700a5e67
Redd Knight <reddknight@ReddsMacBookPro.local>
2017-02-21 16:43:03 -0600

    changed mongodb to EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl : 'mongodb://clmccarley:c3rberus@ec2-54-202-3-122.us-west-2.compute.amazonaws.com:27017/dummyDB'
+    //localUrl: 'mongodb://localhost/meanstacktutorials'
 };
